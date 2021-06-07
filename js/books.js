0f46372005270b6191a00ba0f9333a247759c2e0
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-07 16:42:32 -0500

    Feat: finished create form

--- js/books.js
+++ js/books.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-
-
     function getBooks() {
         let search = document.getElementById("search").value;
         let apply = document.getElementById("apply");
-        let url = document.getElementById("url");
         console.log(search);
-
-
         $.ajax({
             url:
                 "https://www.googleapis.com/books/v1/volumes?q="+
@@ -15,14 +10,13 @@ $(document).ready(function() {
                 "&key="+
                 APIv3Key,
             dataType: "json",
-
             success: function (book) {
                 console.log(book);
                 for(var i = 0; i < book.items.length; i++) {
                     apply.innerHTML += "<p class='select'>" + book.items[i].volumeInfo.title + "</p>";
                     apply.innerHTML += "<h3 class='select'>" + book.items[i].volumeInfo.authors + "</h3>";
                     apply.innerHTML += "<img class='select' src='" + book.items[i].volumeInfo.imageLinks.thumbnail + "'>";
-
+                    apply.innerHTML += "<h4 class='select'>" + book.items[i].volumeInfo.description + "</h4>";
                 }
             },
             type: "GET"
@@ -30,23 +24,23 @@ $(document).ready(function() {
     }
     let url = document.getElementById("url");
     let title = document.getElementById("title");
-
+    let info = document.getElementById("info");
     var submitButton = document.querySelector('#submit');
     submitButton.addEventListener('click', getBooks);
-
     $(document).on("click", "img.select" , function() {
-        var href = $(this).attr('src');
+        let href = $(this).attr('src');
         url.setAttribute("value", href)
     });
-
     $(document).on("click", "p.select" , function() {
         let bookName = $('p.select').clone().html();
         title.setAttribute("value", bookName)
     });
-
+    $(document).on("click", "h4.select" , function() {
+        let description = $('h4.select').clone().html();
+        info.setAttribute("value", description)
+    });
     // $(document).on("click", "h3.select" , function() {
     //     var author = $('h3.select').clone().html();
     //
     // });
-
 });
