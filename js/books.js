299b9adcb8c524273526be0a00f08321d0eddc45
Nathan Adcock <nathan.adcock43@gmail.com>
2021-06-08 00:01:57 -0500

    Feat: got the cards to return in a flex grid from api call

--- js/books.js
+++ js/books.js
@@ -13,10 +13,14 @@ $(document).ready(function() {
             success: function (book) {
                 console.log(book);
                 for(var i = 0; i < book.items.length; i++) {
-                    apply.innerHTML += "<p class='select'>" + book.items[i].volumeInfo.title + "</p>";
-                    apply.innerHTML += "<h3 class='select'>" + book.items[i].volumeInfo.authors + "</h3>";
-                    apply.innerHTML += "<img class='select' src='" + book.items[i].volumeInfo.imageLinks.thumbnail + "'>";
-                    apply.innerHTML += "<h4 class='select'>" + book.items[i].volumeInfo.description + "</h4>";
+                    // apply.innerHTML += "<p class='select'>" + book.items[i].volumeInfo.title + "</p>";
+                    // apply.innerHTML += "<h3 class='select'>" + book.items[i].volumeInfo.authors + "</h3>";
+                    // apply.innerHTML += "<img class='select' src='" + book.items[i].volumeInfo.imageLinks.thumbnail + "'>";
+                    // apply.innerHTML += "<h4 class='select'>" + book.items[i].volumeInfo.description + "</h4>";
+
+
+                    apply.innerHTML += "<div class='col'><div class=\"container grid-body select\" id=\"cardBody\"><div class=\"card\" style=\"width: 18rem;\"><div class=\"card-body\" style=\"background-color: #7A7072\"><h5 class=\"card-title select\" style=\"text-align: center; color: white; font-size: larger; font-weight: bold\">" + book.items[i].volumeInfo.title + "</h5><img  class=\" select\" src=" + book.items[i].volumeInfo.imageLinks.thumbnail + "alt=\"pic\" style=\"width: 250px;\"></div></div></div></div>";
+
                 }
             },
             type: "GET"
@@ -27,22 +31,23 @@ $(document).ready(function() {
     let info = document.getElementById("info");
     var submitButton = document.querySelector('#submit');
     submitButton.addEventListener('click', getBooks);
-    $(document).on("click", "img.select" , function() {
+
+    $(document).on("click", "div.select" , function() {
         let href = $(this).attr('src');
         url.setAttribute("value", href)
     });
-    $(document).on("click", "p.select" , function() {
-        let bookName = $('p.select').clone().html();
+    $(document).on("click", "div.select" , function() {
+        let bookName = $('h5.select').clone().html();
         title.setAttribute("value", bookName)
     });
-    $(document).on("click", "h4.select" , function() {
-        let description = $('h4.select').clone().html();
-        info.setAttribute("value", description)
-    });
+    // $(document).on("click", "h4.select" , function() {
+    //     let description = $('h4.select').clone().html();
+    //     info.setAttribute("value", description)
+    // });
 
-    $(document).on("click", "h4.select" , function() {
-
-    }
+    // $(document).on("click", "h4.select" , function() {
+    //
+    // }
     // $(document).on("click", "h3.select" , function() {
     //     var author = $('h3.select').clone().html();
     //
